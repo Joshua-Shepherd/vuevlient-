89f8e49555377006286b45bcc0961ed7554c7dbb
joshuahiskraken <never.noes.best@gmail.com>
2018-06-17 23:49:13 -0500

    https://www.youtube.com/watch?v=Fa4cRMaTDUI 44:41 backend ready to connect

---
+++ vueclient/server/src/app.js
@@ -0,0 +1,17 @@
+console.log('hellow')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+
+const app = express();
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+app.get('/status', (res,req)=>{
+    res.setEncoding({
+        message: 'Hello'
+    })
+});
+
