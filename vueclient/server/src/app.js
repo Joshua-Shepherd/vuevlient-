cb8731b54478d5871e11f3060dd4996862c5a19f
joshuahiskraken <never.noes.best@gmail.com>
2018-06-23 02:06:30 -0500

    Server

--- vueclient/server/src/app.js
+++ vueclient/server/src/app.js
@@ -1,17 +1,18 @@
-console.log('hellow')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
-
 const app = express();
-app.use(morgan('combine'))
+const port = process.env.PORT || 8081;
+
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/status', (res,req)=>{
-    res.setEncoding({
+
+app.get('/status', (req ,res)=>{
+    res.send({
         message: 'Hello'
     })
 });
 
+app.listen(port, () => console.log('Listening on port'+ ' ' + port));
